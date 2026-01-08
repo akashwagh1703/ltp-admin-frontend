00928f3b229e457cd582372480cc1185cfca1b98
Akash Wagh <Akash.Wagh@esds.co.in>
2026-01-08 17:26:25 +0530

    Get master

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ltp-admin-frontend">
+    <BrowserRouter basename="/admin">
       <App />
     </BrowserRouter>
   </React.StrictMode>
